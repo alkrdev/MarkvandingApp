529a2d68e592a34d238d40abeff1b8452e105336
BjornGrylls <bjornskole@gmail.com>
2022-03-08 12:45:46 +0100

    Mark

--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -1,21 +1,30 @@
 import React from "react"
-import { Text, View, StyleSheet, Platform, Dimensions, TouchableOpacity, TouchableHighlight, Image, Alert } from "react-native"
+import { Text, View, StyleSheet, Platform, Dimensions, TouchableOpacity, TouchableHighlight, Image, Alert, Grid } from "react-native"
 import Constants from "expo-constants"
+import mark from "../images/mark.png"
 
 const Frontpage = () => {
 
     return (
         <View style={styles.container}>
             <Text>HELLO FROM FRONTPAGE</Text>
+            <Image source={mark} style={styles.mark} />
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    text: {
+
+    },
+    mark: {
+        maxHeight: 20
+    },
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
     },
 });
 
