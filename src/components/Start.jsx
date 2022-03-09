7a9fd8ae1eb5862903a770e7efa3999e116e4db0
Alexander Kristensen <ak@kk-partner.dk>
2022-03-09 14:29:53 +0100

    Start + Time Page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Text, View, StyleSheet, Platform, Button, Dimensions, TouchableOpacity, TouchableHighlight, Image, Alert, TextInput } from "react-native"
 import Constants from "expo-constants"
 import CheckBox from 'expo-checkbox';
@@ -9,6 +9,9 @@ var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full height
 
 const Start = (props) => {
+    const [hydrantOpened, setHydrantOpened] = useState(false)
+    const [inGear, setInGear] = useState(false)
+    const [hosePlaced, setHosePlaced] = useState(false)
 
     return (
         <View style={styles.container}>
@@ -24,18 +27,18 @@ const Start = (props) => {
                 <Button title="Brug nuværende lokation"></Button>
 
                 <View style={{ display: "flex", flexDirection: "row" }}>
-                    <CheckBox style={{ marginRight: 5 }}></CheckBox>
+                    <CheckBox value={hydrantOpened} style={{ marginRight: 5 }} onValueChange={() => setHydrantOpened(!hydrantOpened) }/>
                     <Text style={styles.label}>Hydrant Åben</Text>
                 </View>
                 <View style={{ display: "flex", flexDirection: "row" }}>
-                    <CheckBox style={{ marginRight: 5 }}></CheckBox>
+                    <CheckBox value={inGear} style={{ marginRight: 5 }} onValueChange={() => setInGear(!inGear)} />
                     <Text style={styles.label}>Maskine er sat i gear</Text>
                 </View>
                 <View style={{ display: "flex", flexDirection: "row" }}>
-                    <CheckBox style={{ marginRight: 5 }}></CheckBox>
+                    <CheckBox value={hosePlaced} style={{ marginRight: 5 }} onValueChange={() => setHosePlaced(!hosePlaced)} />
                     <Text style={styles.label}>Slange korrekt placeret</Text>
                 </View>
-                <Button title="Start Vander"></Button>
+                <Button title="Start Vander" onPress={() => {if (!hydrantOpened || !inGear || !hosePlaced) return; props.navigation.navigate('Time', {})}}></Button>
 
             </View>
             <Header title={"Start"} navigation={props.navigation} />
--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,21 +1,37 @@
 import React from "react"
-import { Text, View, StyleSheet, Platform, Dimensions, TouchableOpacity, TouchableHighlight, Image, Alert } from "react-native"
+import { Text, View, StyleSheet, Platform, Dimensions, TouchableOpacity, TouchableHighlight, TextInput, Button, Image, Alert } from "react-native"
 import Constants from "expo-constants"
+
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
 
 const Time = (props) => {
 
     return (
         <View style={styles.container}>
-            <Text>HELLO FROM TIME</Text>
+            <Text style={{fontSize: 60}}>Indtast tid for Vander 5</Text>
+            <View>
+                <Text>Tid Tilbage</Text>
+
+                <TextInput style={{ borderWidth: 1, borderStyle: "solid" }}></TextInput>
+                <Text>Færdig kl.</Text>
+
+                <TextInput style={{ borderWidth: 1, borderStyle: "solid" }}></TextInput>
+            </View>
+            <Button title="GEM" onPress={() => { props.navigation.navigate('Frontpage', {})}}></Button>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+        display: "flex",
+        margin: 15,
+        justifyContent: "space-around",        
+        height: height,
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
     },
 });
 
