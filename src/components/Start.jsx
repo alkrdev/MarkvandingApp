bc0e9ea9d9afcc1e643428bbacef0a996dccdb41
Alexander Kristensen <ak@kk-partner.dk>
2022-03-09 13:45:16 +0100

    Start initial page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,12 +1,44 @@
 import React from "react"
-import { Text, View, StyleSheet, Platform, Dimensions, TouchableOpacity, TouchableHighlight, Image, Alert } from "react-native"
+import { Text, View, StyleSheet, Platform, Button, Dimensions, TouchableOpacity, TouchableHighlight, Image, Alert, TextInput } from "react-native"
 import Constants from "expo-constants"
+import CheckBox from 'expo-checkbox';
+import Header from "./Header"
 
-const Start = () => {
+import field from "./../images/mark.png"
+var width = Dimensions.get('window').width; //full width
+var height = Dimensions.get('window').height; //full height
+
+const Start = (props) => {
 
     return (
         <View style={styles.container}>
-            <Text>HELLO FROM START</Text>
+            <View style={styles.form}>
+                <Text style={{ fontSize: 45 }}>Start Vander</Text>
+
+                <Text>Navn på vander</Text>
+
+                <TextInput style={{ borderWidth: 1, borderStyle: "solid" }}></TextInput>
+
+                <Image source={field} style={{ height: 300 }}></Image>
+
+                <Button title="Brug nuværende lokation"></Button>
+
+                <View style={{ display: "flex", flexDirection: "row" }}>
+                    <CheckBox style={{ marginRight: 5 }}></CheckBox>
+                    <Text style={styles.label}>Hydrant Åben</Text>
+                </View>
+                <View style={{ display: "flex", flexDirection: "row" }}>
+                    <CheckBox style={{ marginRight: 5 }}></CheckBox>
+                    <Text style={styles.label}>Maskine er sat i gear</Text>
+                </View>
+                <View style={{ display: "flex", flexDirection: "row" }}>
+                    <CheckBox style={{ marginRight: 5 }}></CheckBox>
+                    <Text style={styles.label}>Slange korrekt placeret</Text>
+                </View>
+                <Button title="Start Vander"></Button>
+
+            </View>
+            <Header title={"Start"} navigation={props.navigation} />
         </View>
     )
 }
@@ -17,6 +49,17 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
     },
+    label: {
+
+    },
+    form: {
+        height: height * 0.85,
+        display: "flex", 
+        flexDirection: "column",
+        justifyContent: "space-between",
+        margin: 25
+    }
+
 });
 
 export default Start
